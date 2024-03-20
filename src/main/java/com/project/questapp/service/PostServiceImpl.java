ce3e92cccb17ad16c7de03e250e5bdb3bffd8c15
Mgokayay <aymustafagokay@gmail.com>
2024-03-20 20:45:30 +0300

    service implementations created

---
+++ src/main/java/com/project/questapp/service/PostServiceImpl.java
@@ -0,0 +1,69 @@
+package com.project.questapp.service;
+
+import com.project.questapp.dto.PostResponseDto;
+import com.project.questapp.dto.PostUpdateReq;
+import com.project.questapp.entity.Post;
+import com.project.questapp.entity.User;
+import com.project.questapp.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostServiceImpl implements PostService{
+
+    private PostRepository postRepository;
+    private UserService userService;
+    @Autowired
+    public PostServiceImpl(PostRepository postRepository, UserService userService) {
+        this.postRepository = postRepository;
+        this.userService = userService;
+    }
+
+    @Override
+    public List<Post> findAll(Optional<Long> userId) {
+        if(userId.isPresent()){
+            return postRepository.findByUserId(userId.get());
+        }
+        return postRepository.findAll();
+    }
+
+    @Override
+    public Post save (PostResponseDto newPostRes) {
+        //TODO Make Converter Here
+        User user = userService.findById(newPostRes.getUserId());
+        if (user == null){
+            return null;
+        }
+        Post postSave = new Post();
+        postSave.setText(newPostRes.getText());
+        postSave.setTitle(newPostRes.getTitle());
+        postSave.setUser(user);
+        return postRepository.save(postSave);
+        //TODO Make Exceptions
+    }
+
+    @Override
+    public Post delete(Long id) {
+        Post post = findById(id);
+        if (post != null){
+            postRepository.delete(post);
+        }
+        return null;
+    }
+
+    @Override
+    public Post findById(Long postId) {
+        return postRepository.findById(postId).orElse(null);
+    }
+
+    @Override
+    public Post update(PostUpdateReq postUpdateReq, Long id) {
+        Post existingUser = findById(id);
+        existingUser.setText(postUpdateReq.getText());
+        existingUser.setTitle(postUpdateReq.getTitle());
+        return postRepository.save(existingUser);
+    }
+}
